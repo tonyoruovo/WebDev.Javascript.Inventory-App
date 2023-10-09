789f495e1b467596d80db0597881516ee59c254f
MathAid <tonyoruovo@gmail.com>
2023-10-09 13:47:50 +0100

    In development

--- server/routes/employee.cjs
+++ server/routes/employee.cjs
@@ -4,13 +4,13 @@ const { get, init, start, commit, dlt, post, put, abort } = require("../controll
  */
 const router = require("express").Router();
 
-router.route("/employee/:id").get(require("../controllers/middlewares/dbInit.cjs"), get);
-router.route("/employee/init").post(require("../controllers/middlewares/dbInit.cjs"), init);
-router.route("/employee/start").post(require("../controllers/middlewares/dbInit.cjs"), start);
-router.route("/employee/commit").post(require("../controllers/middlewares/dbInit.cjs"), commit);
-router.route("/employee/abort").post(require("../controllers/middlewares/dbInit.cjs"), abort);
-router.route("/employee/put").put(require("../controllers/middlewares/dbInit.cjs"), put);
-router.route("/employee/add").post(require("../controllers/middlewares/dbInit.cjs"), post);
-router.route("/employee/delete").delete(require("../controllers/middlewares/dbInit.cjs"), dlt);
+router.route("/employee/:id").get(get);
+router.route("/employee/init").post(init);
+router.route("/employee/start").post(start);
+router.route("/employee/commit").post(commit);
+router.route("/employee/abort").post(abort);
+router.route("/employee/put").put(put);
+router.route("/employee/add").post(post);
+router.route("/employee/delete").delete(dlt);
 
 module.exports = router;
